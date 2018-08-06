5788b65a02cbd7a654b14d927e2de50f7b346d38
Ruiqi Mao <iamruiqi@gmail.com>
2018-08-06 13:41:27 -0400

    Functional test now checks for input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -18,14 +20,29 @@ class NewVisitorTest(unittest.TestCase):
 
         # User checks that page title and header display to-do list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the Test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # User is asked to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attributes('placeholder'),
+            'Enter a to-do item'
+        )
 
         # User types "Buy Groceries"
+        inputbox.send_keys('Buy Groceries')
 
         # When user hits enter, the page lists
         # "1: Buy Groceries" as an item in a to-do list
+        inputbox.send_keys(keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Buy Groceries' for row in rows)
+        )
 
         # The text box remains, asking to enter another item.
         # User enters another item "Cook Dinner"
@@ -37,6 +54,8 @@ class NewVisitorTest(unittest.TestCase):
         # User visits url and sees list is still there
 
         # User Quits the browser
+        self.fail('Finish the Test!')
+
 
 if __name__=='__main__':
     unittest.main(warnings='ignore')
