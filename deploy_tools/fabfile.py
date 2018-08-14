65e10f1d78bc1097c6a22990af0d94a854af0b34
Ruiqi Mao <iamruiqi@gmail.com>
2018-08-14 14:43:21 -0400

    fixed error

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -33,7 +33,7 @@ def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
     append('.env', f'SITENAME={env.host}')
     current_contents = run('cat .env')
-    if 'DJANGO_SECRET_KEY' no in current_contents:
+    if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
