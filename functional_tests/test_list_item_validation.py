d82c3c861dc889344a787a9fbd0b463b81362953
Ruiqi Mao <iamruiqi@gmail.com>
2018-08-15 21:27:12 -0400

    enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -22,9 +22,10 @@ class ItemValidationTest(FunctionalTest):
 
         self.browser.find_element_by_id('id_new_item').send_keys('Buy Milk')
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy Milk')
 
         # User tries to submit a second blank item
-        self.wait_for_row_in_list_table('1: Buy Milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Receives the same warning on the list page
         self.wait_for(lambda: self.assertEqual(
