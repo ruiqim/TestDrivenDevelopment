30ecd95295b63e1e0440edd6ccd6d081154a7404
Ruiqi Mao <iamruiqi@gmail.com>
2018-08-22 10:00:56 -0400

    fixed fab file error not pulling newest commit

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -20,7 +20,7 @@ def _get_latest_source():
         run('git fetch')
     else:
         run(f'git clone {REPO_URL} .')
-    current_commit = local('git log -n l --format=%H', capture=True)
+    current_commit = local('git log -n 1 --format=%H', capture=True)
     run(f'git reset --hard {current_commit}')
 
 
